8797d8ecfc67fc271fea60a17bb7dfcb35eebbce
Marcelo Boeira <me@marceloboeira.com>
2016-03-10 00:44:28 -0300

    test: Move json response to a fixture

--- spec/spotify/track_spec.cr
+++ spec/spotify/track_spec.cr
@@ -2,7 +2,7 @@ require "./../spec_helper"
 
 describe Spotify::Track do
   it "finds a track by id" do
-    response = "{ \"id\" : \"0wJoRiX5K5BxlqZTolB2LD\", \"name\" : \"Purple Haze\" }"
+    response = Fixture.load("track.json")
     WebMock.stub(:get, "https://api.spotify.com/v1/tracks/0wJoRiX5K5BxlqZTolB2LD").to_return(response)
     track = Spotify::Track.find("0wJoRiX5K5BxlqZTolB2LD")
 
