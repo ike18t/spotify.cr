138f054ac4885ac74d9c005b5c00d2e94dcec35c
Marcelo Boeira <me@marceloboeira.com>
2016-03-10 11:34:46 -0300

    test: Improve scenario context

--- spec/spotify/track_spec.cr
+++ spec/spotify/track_spec.cr
@@ -3,11 +3,13 @@ require "./../spec_helper"
 describe Spotify::Track do
   response = Fixture.load("track.json")
 
-  it "finds a track by id" do
-    WebMock.stub(:get, "https://api.spotify.com/v1/tracks/0wJoRiX5K5BxlqZTolB2LD").to_return(response)
-    track = Spotify::Track.find("0wJoRiX5K5BxlqZTolB2LD")
+  context "when finding" do
+    it "finds a track by id" do
+      WebMock.stub(:get, "https://api.spotify.com/v1/tracks/0wJoRiX5K5BxlqZTolB2LD").to_return(response)
+      track = Spotify::Track.find("0wJoRiX5K5BxlqZTolB2LD")
 
-    track.name.should eq("Purple Haze")
+      track.name.should eq("Purple Haze")
+    end
   end
 
   context "when parsing from JSON" do
