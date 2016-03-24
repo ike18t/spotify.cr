ed17ed7e22b45dcf689ec2e66a5e01a27d68468d
Marcelo Boeira <me@marceloboeira.com>
2016-03-23 23:39:03 -0300

    refactor: Update response http check to Crystal 0.14
    Since Crystal 0.14.x it is possible to use the `success?` method on the
    HTTP::Client::Response.
    
    More: https://github.com/crystal-lang/crystal/commit/6fe3233b2833166f2e11da3cf114e0ef1d201184

--- src/spotify/resource.cr
+++ src/spotify/resource.cr
@@ -5,7 +5,7 @@ module Spotify
     def self.find(id : String)
       response = HTTP::Client.get("https://api.spotify.com/v1/#{@@resource}/#{id}")
 
-      unless (200..299).includes?(response.status_code)
+      unless response.success?
         raise Exceptions::Generic.new("Something went wrong")
       end
 
